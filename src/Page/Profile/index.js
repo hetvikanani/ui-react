e38464fca7e1e5d91f78883d226de951afe7d00e
hetvikanani <hetvipatel3110@gmail.com>
2021-09-07 16:49:19 +0530

    GJB

--- src/Page/Profile/index.js
+++ src/Page/Profile/index.js
@@ -4,30 +4,23 @@ import BasicDetails from "./BasicDetails";
 import { ProfileStyle } from "./style";
 
 const panelData = [
-  {
-    key: "1",
-    header: "Basic Details",
-    body: <BasicDetails />,
-  },
-  {
-    key: "2",
-    header: "Financial Details",
-    body: "dummy",
-  },
-  {
-    key: "3",
-    header: "Contact Details",
-    body: "Contact Details",
-  },
+ "Basic Details",  "Finanicial Details","Contact Details"
 ];
 class Profile extends Component {
+
   render() {
     return (
       <ProfileStyle>
         <Collapse panelData={panelData} />
       </ProfileStyle>
+     
     );
   }
 }
 
 export default Profile;
+
+
+
+// ahi loop fervani 6 like paneldata ma just heading j hse p6i map kri etle 3 var loop frse ema je 
+// call tbnay e time e eni body j aave e rite
